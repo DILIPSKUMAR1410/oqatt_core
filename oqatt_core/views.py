376451707d581a80d0ec33e364de0c91b9adee71
DILIP KUMAR <DILIP@LYTYFY.ORG>
2018-03-07 12:54:11 +0530

    firbase json for prod

--- oqatt_core/views.py
+++ oqatt_core/views.py
@@ -12,7 +12,16 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 from os import path
 # from solc import compile_source
+
 cred = credentials.Certificate('/Users/dk/oqatt_core/oqatt-diva-firebase-adminsdk-smc26-4fbe6b846a.json')
+from django.conf import settings
+
+if settings.DEBUG:
+    # Do something
+    cred = credentials.Certificate('/Users/dk/oqatt_core/oqatt-diva-firebase-adminsdk-smc26-4fbe6b846a.json')
+else:
+	cred = credentials.Certificate('/home/ubuntu/oqatt_core/oqatt-diva-firebase-adminsdk-smc26-4fbe6b846a.json')
+
 # Use the application default credentials
 firebase_admin.initialize_app(cred, {
   'projectId': 'oqatt-diva',
